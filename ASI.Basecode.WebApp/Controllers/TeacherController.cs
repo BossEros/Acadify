1684a74240e83fb818aeb718e4331dd2b4c19186
Dager pogi <dagerdarnayla5@gmail.com>
2025-11-13 03:10:23 +0800

    this is for teacher path with static UI

---
+++ ASI.Basecode.WebApp/Controllers/TeacherController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student_Performance_Tracker.Controllers
+{
+    public class TeacherController : Controller
+    {
+        
+            public IActionResult Dashboard()
+            {
+                return View();
+            }
+
+            public IActionResult Settings()
+            {
+                return View();
+            }
+
+            public IActionResult MyClasses()
+            {
+                return View();
+            }
+
+            public IActionResult Schedule()
+            {
+                return View();
+            }
+
+            public IActionResult Students()
+            {
+                return View();
+            }
+
+            public IActionResult Grades()
+            {
+                return View();
+            }
+        }
+    }
+

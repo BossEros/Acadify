24b91dd504dc8527319ee9138e35e2d0f323afcf
Dave <itschristiandavevilanofficial@gmail.com>
2025-11-13 15:19:11 +0800

    Remove remember me from login backend

--- ASI.Basecode.Services/DTOs/LoginRequest.cs
+++ ASI.Basecode.Services/DTOs/LoginRequest.cs
@@ -4,7 +4,6 @@ public class LoginRequest
 {
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
-    public bool RememberMe { get; set; }
 }
 
 

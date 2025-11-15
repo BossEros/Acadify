673b417a4c37ea0f10a25e41e0e27a99e92368da
cicini09 <johnurcheldizon2016@gmail.com>
2025-11-16 02:28:16 +0800

    Time and Capacity Validations, Couple UI Tweaks

--- ASI.Basecode.WebApp/ViewModels/ClassManagement/CreateClassViewModel.cs
+++ ASI.Basecode.WebApp/ViewModels/ClassManagement/CreateClassViewModel.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ASI.Basecode.WebApp.ViewModels.ClassManagement
 {
-    public class CreateClassViewModel
+    public class CreateClassViewModel : IValidatableObject
     {
         public int Id { get; set; }
 
@@ -37,5 +37,55 @@ namespace ASI.Basecode.WebApp.ViewModels.ClassManagement
         [Required(ErrorMessage = "End time is required")]
         [Display(Name = "End Time")]
         public string EndTime { get; set; } = string.Empty;
+
+        // Time Validation
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!TimeSpan.TryParse(StartTime, out var start))
+            {
+                yield return new ValidationResult(
+                    "Invalid start time format.",
+                    new[] { nameof(StartTime) });
+                yield break;
+            }
+
+            if (!TimeSpan.TryParse(EndTime, out var end))
+            {
+                yield return new ValidationResult(
+                    "Invalid end time format.",
+                    new[] { nameof(EndTime) });
+                yield break;
+            }
+
+            TimeSpan earliest = new TimeSpan(6, 0, 0);
+            TimeSpan latest   = new TimeSpan(23, 0, 0);
+
+            if (start < earliest || start > latest)
+                yield return new ValidationResult(
+                    "Start time must be between 6:00 AM and 10:00 PM.",
+                    new[] { nameof(StartTime) });
+
+            if (end < earliest || end > latest)
+                yield return new ValidationResult(
+                    "End time must be between 7:00 AM and 11:00 PM.",
+                    new[] { nameof(EndTime) });
+
+            var duration = end - start;
+
+            if (duration < TimeSpan.FromMinutes(60))
+                yield return new ValidationResult(
+                    "Class must be at least an hour long.",
+                    new[] { nameof(EndTime) });
+
+            if (duration > TimeSpan.FromHours(6))
+                yield return new ValidationResult(
+                    "Class cannot exceed 6 hours.",
+                    new[] { nameof(EndTime) });
+
+            if (end <= start)
+                yield return new ValidationResult(
+                    "End time must be later than start time.",
+                    new[] { nameof(EndTime) });
+        }
     }
 }
--- ASI.Basecode.WebApp/ViewModels/ClassManagement/EditClassViewModel.cs
+++ ASI.Basecode.WebApp/ViewModels/ClassManagement/EditClassViewModel.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace ASI.Basecode.WebApp.ViewModels.ClassManagement
 {
-    public class EditClassViewModel
+    public class EditClassViewModel : IValidatableObject
     {
         public int Id { get; set; }
 
@@ -38,5 +39,55 @@ namespace ASI.Basecode.WebApp.ViewModels.ClassManagement
         [Required(ErrorMessage = "End time is required")]
         [Display(Name = "End Time")]
         public string EndTime { get; set; } = string.Empty;
+
+        // Time Validation
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!TimeSpan.TryParse(StartTime, out var start))
+            {
+                yield return new ValidationResult(
+                    "Invalid start time format.",
+                    new[] { nameof(StartTime) });
+                yield break;
+            }
+
+            if (!TimeSpan.TryParse(EndTime, out var end))
+            {
+                yield return new ValidationResult(
+                    "Invalid end time format.",
+                    new[] { nameof(EndTime) });
+                yield break;
+            }
+
+            TimeSpan earliest = new TimeSpan(6, 0, 0);
+            TimeSpan latest = new TimeSpan(23, 0, 0);
+
+            if (start < earliest || start > latest)
+                yield return new ValidationResult(
+                    "Start time must be between 6:00 AM and 10:00 PM.",
+                    new[] { nameof(StartTime) });
+
+            if (end < earliest || end > latest)
+                yield return new ValidationResult(
+                    "End time must be between 7:00 AM and 11:00 PM.",
+                    new[] { nameof(EndTime) });
+
+            var duration = end - start;
+
+            if (duration < TimeSpan.FromMinutes(60))
+                yield return new ValidationResult(
+                    "Class must be at least 60 minutes long.",
+                    new[] { nameof(EndTime) });
+
+            if (duration > TimeSpan.FromHours(6))
+                yield return new ValidationResult(
+                    "Class cannot exceed 6 hours.",
+                    new[] { nameof(EndTime) });
+
+            if (end <= start)
+                yield return new ValidationResult(
+                    "End time must be later than start time.",
+                    new[] { nameof(EndTime) });
+        }
     }
 }
